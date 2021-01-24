8c9a265734bf6d1b011a64293f16770d0194a54b
Deepak <deepakthakur2051@gmail.com>
2021-01-24 09:20:40 +0530

    Hashing Password

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const ejs  = require("ejs");
 const mongoose  = require("mongoose");
 // const { stringify } = require("querystring");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const { Console } = require('console');
 
 
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema({
 
 
 // Add plugin before creating the mongoose model as passing a 
-userSchema.plugin(encrypt , {secret : process.env.SECRET , encryptedFields : ["password"]   }); // to specify the fields to encrypt
+// userSchema.plugin(encrypt , {secret : process.env.SECRET , encryptedFields : ["password"]   }); // to specify the fields to encrypt
 
 
  const User  = new mongoose.model("User" , userSchema);
@@ -48,7 +48,7 @@ app.post("/register" , function(req ,res)
 {
     const newUser  = new User({
         email : req.body.username,
-        password : req.body.password
+        password : md5(req.body.password)
     });
     newUser.save(function(err)
     {
@@ -68,7 +68,7 @@ app.post("/register" , function(req ,res)
 app.post("/login", function(req , res)
 {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email : username}, function(err , FoundUser)
     {
         if(err)
